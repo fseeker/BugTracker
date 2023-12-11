ad0a63cd6fd1d0c448108eee2a9f7533082b017a
Fahim Seeker <fahimseeker@gmail.com>
2023-12-11 17:24:05 +0600

    added ngx bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { AppComponent } from './app.component';
   declarations: [
     AppComponent
   ],
-  imports: [
+  imports:[
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
